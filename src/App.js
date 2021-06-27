1014e3b0c068cde4955fab6792b639ec8cf2caa8
abdulfayzasa <abdulfayzsolijonov1@gmail.com>
2021-06-27 18:07:05 +0500

    img was added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
+import Myimg from './img/a.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Abdulfayz Solijonov</h1>
+     <h3>Lorem ipsum dolor sit amet.</h3>
+     <img src={Myimg} alt="my img" />
     </div>
   );
 }
